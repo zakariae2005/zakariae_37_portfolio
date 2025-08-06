a97df2c85e99d4343a6a468d8a04b9741ec9dcf6
zakariae2005 <lzakariae1937@gmail.com>
2025-08-06 18:53:19 +0200

    first commit

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -51,7 +51,7 @@ const Hero = () => {
             </div>
 
             <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
-              Hi, I’m Adrian, a developer based in Croatia with a passion for
+              Hi, I’m Zakariae, a developer based in Morocco with a passion for
               code.
             </p>
 
